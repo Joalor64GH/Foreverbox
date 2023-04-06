2a0347e064e5b5186b26e083e83b581764538379
Joalor64 <rhgarcon@gmail.com>
2023-04-06 10:34:05 -0400

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -80,25 +80,7 @@ var app = new function() {
         name: "20_make",
         color: "828282",
         uniqsnd: !0
-    }], this.bonusarray = [{
-        /*name: "The Choir",
-        src: "v1-b1-choir-hb.mp4",
-        code: "2,4,8,11,15",
-        sound: "bonus-choir",
-        aspire: "aspire"
-    }, {
-        name: "The Sailor",
-        src: "v1-b2-sailor-hb.mp4",
-        code: "10,11,14,15,20",
-        sound: "bonus-sailor",
-        aspire: "aspire-sailor"
-    }, {
-        name: "Santa",
-        src: "v1-b3-santa-hb.mp4",
-        code: "1,9,13,14,16",
-        sound: "bonus-santa",
-        aspire: "aspire-santa"
-    */}];
+    }], this.bonusarray = [/*no bonus lol*/];
     for (var n = 0, o = this.animearray.length; n < o; n++) {
         var a = this.animearray[n].name;
         this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json"
